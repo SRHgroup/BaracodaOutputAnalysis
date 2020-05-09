04a2bcf3c45134ee9efcc8ee1b7c000f55da24db
Annie Borch <annbor@dtu-5cg8091tbf.win.dtu.dk>
2020-05-09 10:50:02 +0200

    make augment function

--- R/augment_data.R
+++ R/augment_data.R
@@ -1,27 +1,56 @@
 
 # function with clean data
-clean_data <- function(data) {
-  # Wrangle data
-  # ------------------------------------------------------------------------------
-  my_data_clean <- data %>%
-    # Remove "...17" column (useless - added from excel import); "HLA_allele" (same as HLA); identifier
-    select(-`...17`, -HLA_allele, -identifier) %>%
-    # replace dots and ( with underscore
-    rename_all(funs(str_replace_all(., "\\.|[(]", "_"))) %>%
-    # replace spaces and ) with nothing
-    rename_all(funs(str_replace_all(., " |[)]", ""))) %>%
-    # replace - with minus
-    # replace spaces and ) with nothing
-    rename_all(funs(str_replace_all(., "-", "minus_"))) %>%
-    # make all lowescases
-    rename_all(funs(str_to_lower(.))) %>%
-    # remove control peptides that has introduced all NAs (present in barracoda file but not in Mupexi file)
-    drop_na(norm_peptide) %>%
-    # rename variables to ease reproducibility from now on
-    rename(count = count_1,
-           masked_p_value =`masked_p_p=1iflogfc<0`,
-           p_value = p,
-           neoepitope_sequence = sequence)
+augment_data <- function(data) {
+
+  my_data_clean_aug <- data %>%
+    # add response column
+    mutate(response = case_when(log_fold_change >= 2 & p_value <= 0.01 & input_1&input_2&input_3 != 0 & count > input_1  ~ "yes",
+                                # everything else does not match any of the previous criterias as is labelled no
+                                TRUE ~ "no")) %>%
+    # add organ column
+    mutate(organ = case_when(str_detect(sample, "TU$") ~ "tumor",
+                             str_detect(sample, "SP$") | str_detect(sample, "^CT26") ~ "spleen")) %>%
+
+    # remove _SP or _TU from sample
+    mutate(sample = case_when(str_detect(sample, "TU$") ~ str_replace(sample, ".{3}$",  " ") ,
+                              str_detect(sample, "SP$") ~ str_replace(sample, ".{3}$", " "),
+                              TRUE ~ as.character(sample))) %>%
+
+    # add treatment column
+    mutate(treatment = case_when(str_detect(sample, "^4T1_19") | str_detect(sample,"^4T1_23") | str_detect(sample,"^4T1_20") |
+                                   str_detect(sample,"^4T1_16") | sample == "CT26_C1" | sample == "CT26_D1" | sample == "CT26_D2" ~ "CPI",
+                                 str_detect(sample,"^4T1_22") | str_detect(sample,"^4T1_17") | str_detect(sample,"^4T1_18") |
+                                   sample == "CT26_C3" | sample == "CT26_C4" | sample == "CT26_D4" ~ "Isotype control")) %>%
+
+    # add cell_line column
+    mutate(cell_line = case_when(str_detect(sample, "^4T1") ~ "4T1", str_detect(sample, "^CT26") ~ "CT26")) %>%
+    # add percent_count.fraction column = barcode_count / sum(barcode_counts) for a particular sample * 100
+    group_by(sample) %>%
+    mutate(percent_count_fraction = count/sum(count)*100) %>%
+    # add estimated_frequency column
+    mutate(estimated_frequency = percent_pe*percent_count_fraction/100) %>%
+
+    # add a sum of the normalized counts column for the significant epitopes (response == yes) to calculate the normalized estimated freq
+    #mutate(count_norm_signif = case_when(response == "yes" ~ sum(count_normalised_edger))) %>%
+    mutate(identifier = paste(neoepitope_sequence, hla, sep = "_"),
+           identifier = paste(identifier, cell_line, sep = "_"))
+
+
+  count_norm_signif <- my_data_clean_aug %>%
+    group_by(sample) %>%
+    filter(response == "yes") %>%
+    summarise(count_norm_signif = sum(count_normalised_edger))
+
+
+  my_data_clean_aug <- full_join(my_data_clean_aug, count_norm_signif) %>%
+
+    # add estimated_frequency_normalized column
+    mutate(estimated_frequency_norm = (count_norm_signif*percent_pe/count_norm_signif)) %>%
+
+    # change estimated_frequency_norm of non-response peptides to 0, as this measure is not relevant
+    mutate(estimated_frequency_norm = case_when(response == "no" ~ 0,
+                                                TRUE ~ as.numeric(estimated_frequency_norm)))
+
 }
 
 
