68b6ef9afa0d3ea5b2ca4e95fca2b712c409df14
Sara Suarez Hernandez <you@example.com>
2020-05-12 13:16:47 +0200

    update data_merge function

--- R/merge_all_data.R
+++ R/merge_all_data.R
@@ -3,15 +3,14 @@
 #' This function takes in baracoda data, mupexi data, smaple information and buyyfyboat informatin
 #' @param baracoda all merge baracoda files
 #' @param mupexi all merge mupexi files
-#' @param sampel_information all information about the samplea in the data
+#' @param sampel_information all information about the sample in the data
 #' @param buffycoat_HLA_information information about HLA in buffycoat
 #' @export
 
 # function to merge
 merge_all_data <- function(all_barracoda =  all_barracoda,
                            all_mupexi  = all_mupexi,
-                          sample_info = sample_info,
-                          buffycoat.HLA_info  = buffycoat.HLA_info)
+                          sample_info = sample_info)
   {
 
   all_barracoda %>% group_by(HLA) %>%
@@ -29,10 +28,8 @@ merge_all_data <- function(all_barracoda =  all_barracoda,
 
   mupexi_barracoda <- left_join(all_barracoda, all_mupexi, by = "identifier")
 
-  my_data <- left_join(mupexi_barracoda, sample_info) %>% #bring PE_population info of each sample into barracoda_mupexi file
-    # add HLA_match column if HLA in any of the buffycoat_HLA info file
-    mutate(HLA_match = case_when(HLA %in% buffycoat.HLA_info$HLA1 | HLA %in% buffycoat.HLA_info$HLA2 | HLA %in% buffycoat.HLA_info$HLA3~"yes",
-                                 TRUE ~ "no"))
+  my_data <- left_join(mupexi_barracoda, sample_info)
+
 }
 
 
