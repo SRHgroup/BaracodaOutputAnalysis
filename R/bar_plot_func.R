91fee558ee38ffd6396df2848b9ee589db707642
Annie Borch <annbor@dtu-5cg9424g9b.win.dtu.dk>
2020-05-13 14:26:27 +0200

    update

--- R/bar_plot_func.R
+++ R/bar_plot_func.R
@@ -13,7 +13,6 @@ bar_plot_func <- function(data = my_data_clean_aug,
     filter(str_length(mut_peptide)==pep_length,mutation_consequence=="M") %>%
     ggplot(aes(x=peptide_position)) +
     geom_bar(aes(fill = response), stat = "count")+
-    scale_y_log10() +
     scale_x_discrete(limits = factor(1:pep_length)) +
     scale_fill_manual(values = c("#91bfdb","#ef8a62")) +
     theme_bw() +
